53151b36e88837d01641e57751c6fc251b0836d6
Ritik Yadav <137802777+ritikonweb@users.noreply.github.com>
2026-01-10 12:17:52 +0530

    Delete app directory

--- app/layout.js
+++
@@ -1,8 +0,0 @@
-
-export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <body style={{ margin: 0, fontFamily: 'system-ui, sans-serif' }}>{children}</body>
-    </html>
-  );
-}
